caa8c842c2ac6b33eec6d9300c031707f60bb21f
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-05-07 13:20:20 -0400

    loop practice

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const writeCards = (names) => {
+    let thankYous = [];
+    for (name in names) {
+        thankYous.push(`Thank you, ${names[name]}, for the wonderful surprise gift!`)
+    }
+    return thankYous
+}
+
+const countDown = (x) => {
+    while (x >= 0) {
+        console.log(x)
+        --x
+    }
+
+}
